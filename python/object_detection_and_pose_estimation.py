d8d62a9019c7b3d065656bff82d3e22dad4536ff
tusen takk <tusen.takk.2024@outlook.jp>
2025-02-23 00:22:19 +0900

    modified object_detection_and_pose_estimation.py

--- python/object_detection_and_pose_estimation.py
+++ python/object_detection_and_pose_estimation.py
@@ -12,7 +12,24 @@ from gi.repository import Gst, GLib, GObject
 def tutorial_main():
     Gst.init(sys.argv[1:])
 
-    pipeline = Gst.parse_launch("filesrc location=../resources/detection0.mp4 name=source ! qtdemux ! h264parse ! avdec_h264 max-threads=2  ! videoscale name=source_videoscale n-threads=2 ! videoconvert n-threads=3 name=source_convert qos=false ! video/x-raw, format=RGB, pixel-aspect-ratio=1/1 ! videoscale name=inference_videoscale n-threads=2 qos=false ! video/x-raw, pixel-aspect-ratio=1/1 ! videoconvert name=inference_videoconvert n-threads=2 ! hailonet name=inference_hailonet hef-path=../resources/mspn_regnetx_800mf.hef scheduling-algorithm=1 scheduler-threshold=5 scheduler-timeout-ms=100 vdevice-group-id=1 ! hailofilter name=inference_hailofilter name=pose-estimation so-path=../resources/libmspn_post.so config-path=../resources/mspn.json qos=false ! hailooverlay name=hailo_display_hailooverlay ! videoconvert name=hailo_display_videoconvert n-threads=2 qos=false ! fpsdisplaysink name=hailo_display video-sink=xvimagesink sync=true text-overlay=false signal-fps-measurements=true")
+    pipeline = Gst.parse_launch(" \
+        filesrc location=./resources/example.mp4 name=source ! \
+        qtdemux ! \
+        h264parse ! \
+        avdec_h264 max-threads=2 ! \
+        videoscale name=source_videoscale n-threads=2 ! \
+        videoconvert n-threads=3 name=source_convert qos=false ! \
+        video/x-raw, format=RGB, pixel-aspect-ratio=1/1 ! \
+        videoscale name=inference_videoscale n-threads=2 qos=false ! \
+        video/x-raw, pixel-aspect-ratio=1/1 ! \
+        videoconvert name=inference_videoconvert n-threads=2 ! \
+        hailonet name=inference_hailonet hef-path=./resources/mspn_regnetx_800mf.hef \
+        scheduling-algorithm=1 scheduler-threshold=5 scheduler-timeout-ms=100 vdevice-group-id=1 ! \
+        hailofilter name=pose-estimation so-path=./resources/libmspn_post.so config-path=./resources/mspn.json qos=false ! \
+        hailooverlay name=hailo_display_hailooverlay ! \
+        videoconvert name=hailo_display_videoconvert n-threads=2 qos=false ! \
+        fpsdisplaysink name=hailo_display video-sink=xvimagesink sync=true text-overlay=false signal-fps-measurements=true \
+    ")
     ret = pipeline.set_state(Gst.State.PLAYING)
     if ret == Gst.StateChangeReturn.FAILURE:
         print("Unable to set the pipeline to the playing state.")
